da916361520d575bd376fdb5bff312c1432e5cac
ManishBisht <manishbisht9711@gmail.com>
2021-11-05 21:59:11 +0530

    api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import "./App.css";
+//     const key = "b8097d6d4401dc702b177bb973d9e8de";
+import React from "react";
+import styled from "styled-components";
 
 function App() {
+  const res = fetch(
+    "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=b8097d6d4401dc702b177bb973d9e8de"
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
   return (
     <>
-      <p>manish</p>
+      <Mainbox>
+        <p>
+          Temp <span>{res[0].main.temp}</span>
+        </p>
+      </Mainbox>
     </>
   );
 }
-
 export default App;
+
+const Mainbox = styled.div``;
